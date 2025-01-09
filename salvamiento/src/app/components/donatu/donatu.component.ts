81c5921af728a24646ac911e03513f7932fd604e
2AW3 User 1 <2AW3-1@yourdomain.com>
2025-01-09 12:09:40 +0100

    Aldaketa gehiago

--- salvamiento/src/app/components/donatu/donatu.component.ts
+++ salvamiento/src/app/components/donatu/donatu.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./donatu.component.scss']
 })
 export class DonatuComponent {
+  izena: string = '';
+  abizena: string = '';
+  email: string = '';
+  kantitatea: number = 0;
+  kontua: string = '';
 
+  onSubmit(form: any): void {
+    if (form.valid) {
+      console.log('Formulario enviado con éxito', form.value);
+      // Aquí puedes realizar cualquier acción adicional, como enviar los datos a un servidor.
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
--- salvamiento/src/app/components/kontaktua/kontaktua.component.ts
+++ salvamiento/src/app/components/kontaktua/kontaktua.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./kontaktua.component.scss']
 })
 export class KontaktuaComponent {
+  izena: string = '';
+  abizena: string = '';
+  email: string = '';
+  nota: string = '';
+  iradokizuna: string = '';
 
+  onSubmit(kontaktuaForm: any): void {
+    if (kontaktuaForm.valid) {
+      console.log('Formulario válido:', kontaktuaForm.value);
+      alert('Formulario bidali da!');
+    } else {
+      console.error('Formulario ez da zuzena:', kontaktuaForm);
+      alert('Erroreak daude formularioan.');
+    }
+  }
 }
